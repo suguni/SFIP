d699ed0ab1bb0491a34998dec12b033ca0152e13
Steve Yu <suguni@gmail.com>
2014-05-07 13:28:00 +0900

    modify code

--- ex2.scala
+++ ex2.scala
@@ -1,53 +1,58 @@
-// 1.
-def signum(n:Int) : Int = if (n == 0) 0 else if (n > 0) 1 else -1
+object ex2 {
+  // 1.
+  def signum(n:Int) : Int = if (n == 0) 0 else if (n > 0) 1 else -1
 
-// 2
-val a = -1
-val x = if (a > 0) 10
-// => x: AnyVal = ()
-// 이니까 Unit 인 듯
+  // 2
+  val a = -1
+  val x = if (a > 0) 10
+  // => x: AnyVal = ()
+  // 이니까 Unit 인 듯
 
-// 3
-// x를 선언하기 위해? 모르겠는걸...
+  // 3
+  // x를 선언하기 위해? 모르겠는걸...
 
-// 4
-for (i <- 10 to 0 by -1) println(i)
+  // 4
+  for (i <- 10 to 0 by -1) println(i)
 
-// 5
-def countdown(n:Int) {
-  if (n >= 0) {
-    print(n + " ")
-    countdown(n-1)
+  // 5
+  def countdown(n:Int) {
+    if (n >= 0) {
+      print(n + " ")
+      countdown(n-1)
+    }
   }
-}
 
-// 6
-def product(str:String):Long = {
-  var p:Long = 1
-  for (i <- str) p *= i.toInt;
-  p
-}
+  // 6
+  /*
+   def product(str:String):Long = {
+   var p:Long = 1
+   for (i <- str) p *= i.toInt;
+   p
+   }
+   */
 
-// 7
-"Hello".foldLeft(1L)((x:Long, y:Char) => x * y)
+  // 7
+  "Hello".foldLeft(1L)((x:Long, y:Char) => x * y)
 
-// 8
-def product(str:String):Long = {
-  var p:Long = 1
-  for (i <- "Hello") p *= i.toInt;
-  p
-}
+  // 8
+  def product(str:String):Long = {
+    var p:Long = 1
+    for (i <- "Hello") p *= i.toInt;
+    p
+  }
 
-// 9
-def product2(str:String):Long = {
-  if (str.length == 0) 1
-  else str.head * product2(str.tail)
-}
+  // 9
+  def product2(str:String):Long = {
+    if (str.length == 0) 1
+    else str.head * product2(str.tail)
+  }
 
-// 10
-def pow1(x:Long, n:Long):Float = {
-  if (n == 0) 1L
-  else if (n < 0) 1 / pow1(x, -n)
-  else if (n % 2 == 0) pow1(x, n / 2) * pow1(x, n / 2)
-  else x * pow1(x, n - 1)
+  // 10
+  def pow1(x:Long, n:Long):Float = {
+    if (n == 0) 1L
+    else if (n < 0) 1 / pow1(x, -n)
+    else if (n % 2 == 0) pow1(x, n / 2) * pow1(x, n / 2)
+    else x * pow1(x, n - 1)
+  }
+
 }
--- ex3.scala
+++ ex3.scala
@@ -1,67 +1,91 @@
-// 1
-val n = 10
-val a = new Array[Int](n)
-for (i <- 0 until a.length) a(i) = (math.random * n).toInt
+object ex3 {
 
-// 2
-val a = Array(1, 2, 3, 4, 5)
-for (i <- 0 until a.length by 2) {
-  if (i + 1 < a.length) {
-    val t = a(i)
-    a(i) = a(i + 1)
-    a(i + 1) = t
+  object p1 {
+    // 1
+    val n = 10
+    val a = new Array[Int](n)
+    for (i <- 0 until a.length) a(i) = (math.random * n).toInt
+  }
+
+  object p2 {
+    // 2
+    val a = Array(1, 2, 3, 4, 5)
+    for (i <- 0 until a.length by 2) {
+      if (i + 1 < a.length) {
+        val t = a(i)
+        a(i) = a(i + 1)
+        a(i + 1) = t
+      }
+    }
+  }
+
+  object p3 {
+    // 3
+    val n = 9
+    val a = new Array[Int](n)
+    for (i <- 0 until a.length) a(i) = (math.random * n).toInt
+
+    val b = for (i <- 0 until a.length) yield {
+      if (i % 2 == 0) {
+        if (i + 1 < a.length) a(i + 1)
+        else a(i)
+      } else a(i - 1)
+    }
+  }
+
+  object p4 {
+    // 4
+    val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3)
+    // result : Array(1, 3, 4, 9, 1, -2, 0, -3, 0, -3)
+    val pos_idx = for (i <- 0 until a.length if a(i) > 0) yield i
+    val nz_idx = for (i <- 0 until a.length if a(i) <= 0) yield i
+    val bf = pos_idx ++: nz_idx
+    val result = for (i <- 0 until bf.length) yield a(bf(i))
+  }
+
+  object p5 {
+    // 5
+    val a = Array[Double](1.2, 2.3, 4, 5.2)
+    // average: 3.175
+    val avg = a.reduce(_ + _) / a.length
+  }
+
+  object p6 {
+    // 6
+    val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3)
+    // array reverse sort
+    a.sorted.reverse
+
+    // ArrayBuffer reverse sort
+    val b = a.toBuffer
+    b.sorted.reverse
+  }
+
+  object p7 {
+    // 7
+    val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3)
+    a.distinct
+  }
+
+  object p8 {
+
+    // 8
+    val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3).toBuffer
+    val nidx = (for (i <- 0 until a.length if a(i) < 0) yield i).reverse.dropRight(1)
+    for (i <- 0 until nidx.length) a.remove(nidx(i))
+    // result: a => 1 3 -2 4 0 9 1 0
+  }
+
+  object p9 {
+    // 9
+    val prefix = "America/"
+    java.util.TimeZone.getAvailableIDs.filter(_.startsWith(prefix)).map(_.stripPrefix(prefix))
+  }
+
+  object p10 {
+    // 10
+    import java.awt.datatransfer._
+    val flavors = SystemFlavorMap.getDefaultFlavorMap().asInstanceOf[SystemFlavorMap]
+    val image_flavors = flavors.getNativesForFlavor(DataFlavor.imageFlavor)
   }
 }
-
-// 3
-val n = 9
-val a = new Array[Int](n)
-for (i <- 0 until a.length) a(i) = (math.random * n).toInt
-
-val b = for (i <- 0 until a.length) yield {
-  if (i % 2 == 0) {
-    if (i + 1 < a.length) a(i + 1)
-    else a(i)
-  } else a(i - 1)
-}
-
-// 4
-val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3)
-// result : Array(1, 3, 4, 9, 1, -2, 0, -3, 0, -3)
-val pos_idx = for (i <- 0 until a.length if a(i) > 0) yield i
-val nz_idx = for (i <- 0 until a.length if a(i) <= 0) yield i
-val bf = pos_idx ++: nz_idx
-val result = for (i <- 0 until bf.length) yield a(bf(i))
-
-// 5
-val a = Array[Double](1.2, 2.3, 4, 5.2)
-// average: 3.175
-val avg = a.reduce(_ + _) / a.length
-
-// 6
-val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3)
-// array reverse sort
-a.sorted.reverse
-
-// ArrayBuffer reverse sort
-val b = a.toBuffer
-b.sorted.reverse
-
-// 7
-val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3)
-a.distinct
-
-// 8
-val a = Array(1, 3, -2, 4, 0, 9, -3, 1, 0, -3).toBuffer
-val nidx = (for (i <- 0 until a.length if a(i) < 0) yield i).reverse.dropRight(1)
-for (i <- 0 until nidx.length) a.remove(nidx(i))
-// result: a => 1 3 -2 4 0 9 1 0
-
-// 9
-val prefix = "America/"
-java.util.TimeZone.getAvailableIDs.filter(_.startsWith(prefix)).map(_.stripPrefix(prefix))
-
-// 10
-import java.awt.datatransfer._
-val flavors = SystemFlavorMap.getDefaultFlavorMap().asInstanceOf[SystemFlavorMap]
-val image_flavors = flavors.getNativesForFlavor(DataFlavor.imageFlavor)
